10f74d4a7a7f8799cdd9c9e1eefc7e3fde18dadd
Laurens Hagendoorn <Laurens.Hagendoorn@gmail.com>
2016-03-29 00:35:01 +0200

    minibatchkmeans is dus echt VEUL sneller

--- Scripts/Clustering_kMeans.py
+++ Scripts/Clustering_kMeans.py
@@ -6,17 +6,23 @@ Created on Tue Mar 22 23:05:48 2016
 """
 
 import numpy as np
-from sklearn.cluster import KMeans
-import scipy.io as sio
+from sklearn.cluster import MiniBatchKMeans
+#import scipy.io as sio
+import pandas as pd
 
 ###############################################################################
 # Load sample data
-caffeatures = sio.loadmat('C:/Users/Laurens/Documents/TeamGreaterThanBrains/Features/caffe/caffe_features_werktdit.mat')['feats'].transpose()
+#caffeatures = sio.loadmat('C:/Users/Laurens/Documents/TeamGreaterThanBrains/Features/caffe/caffe_features_werktdit.mat')['feats'].transpose()
+testRead = pd.read_csv('C:/Users/Laurens/Documents/uni/MLP/data/caffe_features_train.csv', header=None, nrows = 1)
+caffeatures = pd.read_csv('C:/Users/Laurens/Documents/uni/MLP/data/caffe_features_train.csv', header=None, sep=',', engine='c', dtype={c: np.float64 for c in list(testRead)})
+
 
 ###############################################################################
 # Compute clustering with KMeans
 
-km = KMeans(n_clusters=9)
+print('HOOOOOI')
+#km = KMeans(n_clusters=100, n_jobs = -2)
+km = MiniBatchKMeans(n_clusters=100)
 km.fit(caffeatures)
 labels = km.labels_
 cluster_centers = km.cluster_centers_
@@ -24,20 +30,6 @@ cluster_centers = km.cluster_centers_
 labels_unique = np.unique(labels)
 n_clusters_ = len(labels_unique)
 
+
+
 ###############################################################################
-# Plot result
-import matplotlib.pyplot as plt
-from itertools import cycle
-
-plt.figure(1)
-plt.clf()
-
-colors = cycle('bgrcmykbgrcmykbgrcmykbgrcmyk')
-for k, col in zip(range(n_clusters_), colors):
-    my_members = labels == k
-    cluster_center = cluster_centers[k]
-    plt.plot(caffeatures[my_members, 10], caffeatures[my_members, 11], col + '.')
-    plt.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col,
-             markeredgecolor='k', markersize=14)
-plt.title('Estimated number of clusters: %d' % n_clusters_)
-plt.show()
