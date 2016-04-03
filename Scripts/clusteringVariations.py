d6b1a642e2bd1f05265db9aa60844e37f190f61d
Laurens Hagendoorn <Laurens.Hagendoorn@gmail.com>
2016-04-03 23:37:44 +0200

    zo zou ie moeten werken, al moeten de paths nog voor de server worden aangepast

--- Scripts/clusteringVariations.py
+++ Scripts/clusteringVariations.py
@@ -7,12 +7,14 @@ Created on Fri Apr 01 18:18:32 2016
 
 import numpy as np
 import pandas as pd
+import sys
 
 from dataanal.figureOutValidationSet import getSplit
 from sklearn.mixture import GMM
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.svm import SVC
 from sklearn.externals import joblib
+from sklearn.metrics import f1_score
 
 bizWithoutLabel = [1627, 2661, 2941, 430] #shockingly enough, hardcoding is the most effiient way I can come up with at the moment to not have to load an entirely new csv file
 
@@ -20,11 +22,15 @@ bizWithoutLabel = [1627, 2661, 2941, 430] #shockingly enough, hardcoding is the 
 #testRead = pd.read_csv('C:/Users/Laurens/Documents/uni/MLP/data/features/caffe_features_train.csv', header=None, nrows = 1)
 trainData = pd.read_csv('C:/Users/Laurens/Documents/uni/MLP/data/features/caffe_features_train.csv', header=None, sep=',', engine='c', dtype={c: np.float64 for c in np.ones(4096)})
 data = pd.concat([trainData, pd.read_csv('C:/Users/Laurens/Documents/uni/MLP/data/features/caffe_features_test.csv', header=None, sep=',', engine='c', dtype={c: np.float64 for c in np.ones(4096)}, nrows=100)])
+print('data loaded!')
 
 #------------                     TODO take command line arguments to make this dependend on the instatiation
 #dependend on the instantiation
-n_classes = 10
-cov_type = 'diag'
+classOptions = [2048, 1024, 512, 256, 128, 64]
+covOptions = ['diag', 'spherical', 'tied', 'full']
+arg = sys.argv[0]
+n_classes = classOptions[arg/4]
+cov_type = covOptions[arg%4]
 
 clusterer = GMM(n_components=n_classes, covariance_type=cov_type,n_iter=0)
 clusterer.fit(data)
@@ -52,9 +58,11 @@ trainBizIds, verifBizIds = getSplit()
 photoToBiz = pd.read_csv('C:/Users/Laurens/Documents/uni/MLP/data/train_photo_to_biz_ids.csv', sep=',')    
 photoToBiz = photoToBiz[~photoToBiz.business_id.isin(bizWithoutLabel)] #remove biz without a label
 clusteredTrainSet = pd.DataFrame(index = trainBizIds, columns = range(n_classes))
+clusteredVerifSet = pd.DataFrame(index = verifBizIds, columns = range(n_classes))
 photoToIndex = pd.read_csv('C:/Users/Laurens/Documents/uni/MLP/data/features/photo_order_train.csv', sep=',', header=None).reset_index(level=0)
 
 #COMMENT OUT THE COUNTS WHEN NOT TESTING
+#--------- Cluster the train set
 count = trainBizIds.size;
 for busId in trainBizIds:
     photoIds = photoToBiz.loc[photoToBiz['business_id'] == busId].photo_id.to_frame()
@@ -67,14 +75,39 @@ for busId in trainBizIds:
     clusteredTrainSet.loc[busId] = probs
     count-=1
     print(count)
+    
+#---------- Cluster the verification set
+count = verifBizIds.size;
+for busId in verifBizIds:
+    photoIds = photoToBiz.loc[photoToBiz['business_id'] == busId].photo_id.to_frame()
+    photoIds = photoIds.photo_id.map(lambda x: str(x)+ 'm.jpg')
+    photoIds = photoIds.to_frame()
+    featureIds = pd.merge(photoToIndex, photoIds, left_on=0, right_on='photo_id')['index'] #Select only the indices of the images of this business
+    probs = clusterer.predict_proba(data.iloc[featureIds.values])
+    probs = probs.sum(axis=0, dtype=np.float64)
+    probs /= probs.max()
+    clusteredVerifSet.loc[busId] = probs
+    count-=1
+    print(count)
 
 #=================== Prepare the labelsTrainSet =========================
 binLabels = pd.read_csv('C:/Users/Laurens/Documents/Uni/MLP/Data/labels_train_y.csv', header=None)
 indexToBizId = pd.DataFrame({'bizId': photoToBiz.business_id.unique()}).reset_index(level=0)
-indexToBizId = indexToBizId[indexToBizId.bizId.isin(trainBizIds)]
-labelsTrainSet = binLabels.iloc[indexToBizId['index']]
+trainIndexToBizId = indexToBizId[indexToBizId.bizId.isin(trainBizIds)]
+labelsTrainSet = binLabels.iloc[trainIndexToBizId['index']]
 
-S = OneVsRestClassifier(SVC(kernel='poly')).fit(clusteredTrainSet.values, labelsTrainSet)
+#----- fit SVM
+classifier = OneVsRestClassifier(SVC(kernel='poly')).fit(clusteredTrainSet.values, labelsTrainSet)
 
-#------------------            TODO: Classify the validation set, save results
+#================= Calculate validation score ==========================
+verifIndexToBizId = indexToBizId[indexToBizId.bizId.isin(verifBizIds)]
+labelsVerifSet = binLabels.iloc[verifIndexToBizId['index']]
+
+predictions = classifier.predict(clusteredVerifSet)
+
+score = f1_score(labelsVerifSet.values.ravel(), predictions.ravel())
+
+f = open(str(n_classes) + cov_type + 'Score.txt', 'w')
+f.write(str(score))
+f.close()
 
