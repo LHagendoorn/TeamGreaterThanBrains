7fd0b6cff249560e74409d4bd9210684281fb8a3
DiedeKemper <diede.kemper.18@gmail.com>
2016-04-06 17:08:10 +0200

    Notulen skype meeting Maaike. SVM splitsen train en validation set.

--- Scripts/SVM/Aggregator.py
+++ Scripts/SVM/Aggregator.py
@@ -1,11 +1,13 @@
 
 import pandas as pd
+from datetime import datetime
 
 
 class Aggregator:
 
-    def __init__(self, Xdata, features, photo_order):
+    def __init__(self, Xdata, features, photo_order, trainSet=True):
         self.Xdata = Xdata
+        self.trainSet = trainSet #true if X data is trainset, if testset, then false
         self.features = features
         self.photo_order = photo_order
 
@@ -29,7 +31,7 @@ class Aggregator:
         nrOfFeatures = self.features.shape[1]
 
         #pre-allocate space for aggregated data
-        aggregatedData = [0] * nrOfBusinesses * nrOfFeatures * 5
+        aggregatedData = [0] * nrOfBusinesses
 
         #for each business
         for businessID in allBusinessIDs:
@@ -39,7 +41,7 @@ class Aggregator:
             nrOfPhotos = len(photosIDs)
 
             #pre-allocate space for feature vectors of this business
-            businessData = [0] * nrOfPhotos * nrOfFeatures
+            businessData = [0] * nrOfPhotos
 
             #count how many photosIDS did not have a feature vector
             nrOfMissingPhotos = 0
@@ -58,7 +60,8 @@ class Aggregator:
                     #save the feature vector of this photo
                     ind = indx.index[0]
                     feats = self.features.loc[ind,:]
-                    businessData[photoIndex][:] = feats.values.tolist()
+                    ff = feats.values.tolist()
+                    businessData[photoIndex] = ff
                     photoIndex += 1
 
                 else: ##count the missing photo
@@ -68,15 +71,18 @@ class Aggregator:
             businessData = businessData[:-nrOfMissingPhotos][:]
 
             #compute and save aggregated data
-            aggregatedData[businessIndex][:][:] = self.aggregate(businessData)
+            aggregatedData[businessIndex] = self.aggregate(businessData)
             businessIndex += 1
 
             #print progress
             counter = counter + 1
-            if counter/100 == int(counter/100):
-                print(counter)
+            if counter%100 == 0:
+                print('Progress aggregation. Business ' + str(counter))
+                print datetime.now()
 
-        #TODO: save aggregated Data
+        #save aggregatedData to csv files
+        self.save_aggregated_data(aggregatedData)
+
         return aggregatedData
 
     '''
@@ -84,30 +90,33 @@ class Aggregator:
 
     featureVectors = list of size nrOfPhotos x nrOfFeatures
 
-    Returns: list of size nrOfFeatures x 5
+    Returns: list of size nrOfFeatures x 4
     '''
     def aggregate(self, featureVectors):
         df = pd.DataFrame(featureVectors)
 
-        laag = df.min()
         q1 = df.quantile(0.25)
         q2 = df.quantile(0.5)
         q3 = df.quantile(0.75)
         hoog = df.max()
 
-        print('debug: deze waardes moeten 4096 zijn')
-        print(len(laag))
-        print(len(q1))
-        print(len(q2))
-        print(len(q3))
-        print(len(hoog))
-
-        dinges = [laag, q1, q2, q3, hoog]
-        result = pd.concat(dinges, axis=1)
+        dinges = [q1, q2, q3, hoog]
+        result = pd.concat(dinges, axis=0)
 
         return result.values
 
     '''Saves data in csv file '''
-    def save_aggregated_data(self, data, filename):
-        #TODO
-        return True
+    def save_aggregated_data(self, data):
+
+        df = pd.DataFrame(data)
+
+        if self.trainSet:
+            filename = 'Train'
+        else:
+            filename = 'Test'
+
+        #save in csv file
+        df.to_csv('./input/aggData' + filename + '.csv', index=False, header=None)
+
+
+
--- Scripts/SVM/SVM_spreiding.py
+++ Scripts/SVM/SVM_spreiding.py
@@ -6,59 +6,142 @@ Created on Wed Mar 23 12:53:10 2016
 @author: Diede
 """
 
+from datetime import datetime
+import pandas as pd
+import numpy as np
 import LoadData
-import Aggregator
+import CreateClassification as cc
+from Aggregator import Aggregator
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.svm import LinearSVC
 
 class SVM_spreiding:
 
     ''''Initialize SVM_spreiding. Loads the data and computes/loads aggregated data. '''''
-    def __init__(self, loadAggPath=None):
+    def __init__(self, loadAggData):
         print 'Initializing SVM: Loading Data....'
-        self.loadAggPath = loadAggPath #path to aggregated data saved in csv file
+        self.loadAggData = loadAggData #boolean if aggregated data should be loaded
 
         #load general data
-        generalData = LoadData.load()
+        generalData = LoadData.load('input')
         self.Xtrain = generalData['X_TRAIN']
         self.Ytrain = generalData['Y_TRAIN']
         self.Xtest = generalData['X_TEST']
 
-        #if aggregated data needs to be generated, also load caffe features data
-        if self.loadAggPath is None:
-            trainFeatureData = LoadData.load_caffe_features(trainSet=True)
-            testFeatureData = LoadData.load_caffe_features(trainSet=False)
+        print 'Loading general data is done'
+        print datetime.now()
 
+        #if aggregated data needs to be generated, also load caffe features data
+        if not self.loadAggData:
+            #load train data
+            trainFeatureData = LoadData.load_caffe_features('input', trainSet=True)
             self.trainFeatures = trainFeatureData['FEATURES']
             self.trainOrder = trainFeatureData['ORDER']
+
+            #delete validation dictionary from memory
+            del trainFeatureData
+
+            #############################################
+            #   SPLIT TRAINSET INTO TRAIN AND VALIDATION PART
+            ##############################################
+
+            #load business ids of train and validation set
+            trainSetIds = np.load('input/trainSet.npy')
+            validSetIds = np.load('input/verifSet.npy')
+
+            #merge trainOrder with trainFeatures
+            self.trainOrder.rename(columns={0:'photo_id'},inplace=True)
+            allData = pd.concat([self.trainOrder, self.trainFeatures], axis=1)
+
+            #create dataframes of photo indices for train and validation set
+            trainPhotoIds = self.Xtrain[self.Xtrain.business_id.isin(trainSetIds)]
+            validPhotoIds = self.Xtrain[self.Xtrain.business_id.isin(validSetIds)]
+
+            #merge allData dataframe with photo indice dataframes --> split allData in train and validation
+            self.validationFeatures = pd.merge([validPhotoIds, allData], on='photo_id')
+            self.trainFeatures = pd.merge([trainPhotoIds, allData], on='photo_id')
+
+            #split order and feature files
+            self.validationOrder = self.validationFeatures['photo_id']
+            self.trainOrder = self.trainFeatures['photo_id']
+
+            #remove unnecessary columns
+            self.validationFeatures.drop('photo_id', axis=1)
+            self.validationFeatures.drop('business_id', axis=1)
+            self.trainFeatures.drop('photo_id', axis=1)
+            self.trainFeatures.drop('business_id', axis=1)
+
+            #############################################
+            #   COMPUTE AGGREGATED DATA
+            ##############################################
+
+            #compute validation aggregated data
+            self.call_aggregator('validation')
+
+            #delete validation data from memory
+            del self.validationFeatures
+            del self.validationOrder
+
+            print 'VALIDATION SET DONE'
+            print datetime.now()
+
+            #compute train aggregated data
+            self.call_aggregator('train')
+
+            #delete train data from memory
+            del trainFeatureData
+            del self.trainFeatures
+            del self.trainOrder
+
+            print 'TRAIN SET DONE. HALFWAY THERE.'
+            print datetime.now()
+
+            #load test data
+            testFeatureData = LoadData.load_caffe_features('input', trainSet=False)
             self.testFeatures = testFeatureData['FEATURES']
             self.testOrder = testFeatureData['ORDER']
 
-        print 'Initializing SVM: Loading data is done.'
-        #get aggregated data
-        self.initialize_aggregated_data()
+            #compute test aggregated data
+            self.call_aggregator('test')
 
-    ''''Initialize the aggregated data. Either loads it or calls an aggregator to compute it.'''''
-    def initialize_aggregated_data(self):
-        if(self.loadAggPath is not None):
-            self.load_aggregated_data()
+            #delete test data from memory
+            del testFeatureData
+            del self.testFeatures
+            del self.testOrder
         else:
-            self.call_aggregator()
+            self.load_aggregated_data()
+        print 'Initializing SVM: Loading data is done.'
+        print 'KLAAR'
+        print datetime.now()
+
 
     ''''Call aggregator to compute aggregated data'''''
-    def call_aggregator(self):
-        print 'Initializing SVM: Computing aggregated train data...'
-        agg = Aggregator(self.Xtrain, self.trainFeatures, self.trainOrder)
-        self.aggTrainData = agg.get_aggregated_data()
-        print 'Initializing SVM: Computing aggregated test data...'
-        agg = Aggregator(self.Xtest, self.testFeatures, self.testOrder)
-        self.aggTestData = agg.get_aggregated_data()
-        print 'Initializing SVM: Computations are done.'
+    def call_aggregator(self, setString):
+        if setString == 'train':
+            print 'Initializing SVM: Computing aggregated train data...'
+            agg = Aggregator(self.Xtrain, self.trainFeatures, self.trainOrder)
+            self.aggTrainData = agg.get_aggregated_data()
+        elif setString == 'validation':
+            print 'Initializing SVM: Computing aggregated validation data...'
+            agg = Aggregator(self.Xtrain, self.validationFeatures, self.validationOrder)
+            self.aggValData = agg.get_aggregated_data()
+        elif setString == 'test':
+            print 'Initializing SVM: Computing aggregated test data...'
+            agg = Aggregator(self.Xtest, self.testFeatures, self.testOrder)
+            self.aggTestData = agg.get_aggregated_data()
+        else:
+            print 'Error call_aggregator. This type of set is unknown.'
+        print 'Initializing SVM: Computating is done.'
 
-    ''''Load aggregated data from given location'''''
+    ''''Load aggregated data from location'''''
     def load_aggregated_data(self):
-        print 'Initializing SVM: Loading aggregated data...'
-        #TODO
+        print 'Initializing SVM: Loading aggregated data from csv files...'
+        df = pd.read_csv('./input/aggDataTrain.csv')
+        self.aggTrainData = df.values
+        df = pd.read_csv('./input/aggDataTest.csv')
+        self.aggTestData = df.values
+        df = pd.read_cs('./input/aggDataValidation.csv')
+        self.aggValData = df.values
         print 'Initializing SVM: Loading is done.'
 
     def train(self):
@@ -77,9 +160,9 @@ class SVM_spreiding:
         print 'Predicting probabilities is done.'
 
     def createClassification(self, filename):
-        #TODO
-        print 'TODO: create classification'
+        cc.create(self.Ypred, self.Xtest, filename)
+        print 'Classification file created'
 
     def createProbabilities(self, filename):
-        #TODO
-        print 'TODO: create probabilities'
+        cc.createProbFile(self.Yprob, filename)
+        print 'Probabilities file created'
