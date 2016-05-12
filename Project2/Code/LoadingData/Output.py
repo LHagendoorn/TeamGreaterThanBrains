514585d2deaa7c911abeb41aff989dff2f1f87fb
Danielle Tump <DanielleT@154-033.wlan-int.ru.nl>
2016-05-12 16:39:27 +0200

    Creates submission file

---
+++ Project2/Code/LoadingData/Output.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 12 16:12:25 2016
+
+@author: DanielleT
+"""
+from Load import *
+import time
+import numpy
+
+#Makes the submissionfile. Please note:
+#   You need testdata_filenames in the same folder as your code
+#   Data = the probabilities of each class per photo in a pandas dataframe
+#   Submnumber = The number of the submission that day
+#   Name = The name of the approach you have used
+
+def to_outputfile(data,submnumber,name):
+    labels_testdata = load_testdata_filenames()
+    data[data < 0.0000001] = 0.0000001
+    df = pd.DataFrame({ 'img' : numpy.asarray(labels_testdata),
+                    'c0' : predsdf.iloc[:,0],
+                    'c1' : predsdf.iloc[:,1],
+                    'c2' : predsdf.iloc[:,2],
+                    'c3' : predsdf.iloc[:,3],
+                    'c4' : predsdf.iloc[:,4],
+                    'c5' : predsdf.iloc[:,5],
+                    'c6' : predsdf.iloc[:,6],
+                    'c7' : predsdf.iloc[:,7],
+                    'c8' : predsdf.iloc[:,8],
+                    'c9' : predsdf.iloc[:,9]})
+    df = df[['img','c0','c1','c2','c3','c4','c5','c6','c7','c8','c9']]
+    timestr = time.strftime("%Y%m%d")
+    filename = 'outputfile_' + timestr + '_' + str(submnumber) + '_' + name + '.csv'
+    df.to_csv(filename,float_format='%.7f',index=False)
+    
