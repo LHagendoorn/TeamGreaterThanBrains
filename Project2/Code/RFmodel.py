4c31f5e04dfa21dfa2c3054f7fa07a0020155614
Natanael Adityasatria <nata.adit@gmail.com>
2016-05-18 15:41:36 +0200

    Random forest with padded images, slightly better result. The score was 1.79770

--- Project2/Code/RFmodel.py
+++ Project2/Code/RFmodel.py
@@ -15,6 +15,7 @@ df_traindata_lab = Input.load_traindata_labels()
 df_testdata_caf = Input.load_testdata_caffefeatures()
 
 print("--- load data: %s seconds ---" % round((time.time() - start_time),2))
+start_time = time.time()
 
 x_train = df_traindata_caf
 y_train = df_traindata_lab
@@ -25,12 +26,14 @@ rf = RandomForestClassifier(n_estimators=500)
 rf.fit(x_train, y_train)
 
 print("--- train model: %s seconds ---" % round((time.time() - start_time),2))
+start_time = time.time()
 
 # Predict
 preds = rf.predict_proba(x_test)
 predsdf = pd.DataFrame(preds)
 
 print("--- prediction: %s seconds ---" % round((time.time() - start_time),2))
+start_time = time.time()
 
 # Create ouput file
 Output.to_outputfile(predsdf,1,'RF')
