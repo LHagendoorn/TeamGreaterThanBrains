8ef9b3e4ae1123fb1bd8d5d1e1b93332f8f119d8
Natanael Adityasatria <nata.adit@gmail.com>
2016-06-06 14:14:50 +0200

    Added code to seperate trainset and validationset images from traindata

--- Project2/Code/IO/code_to_create_csv_files/check_missing_images.py
+++ Project2/Code/IO/code_to_create_csv_files/check_missing_images.py
@@ -1,13 +1,49 @@
 # -*- coding: utf-8 -*-
 import glob
 import os
-import pandas as pd
+import csv
+from itertools import chain
 
-def check_images():
-#    subdirs = [x[0] for x in os.walk(os.getcwd())]
-#    for subdir in subdirs:
-#        for filename in glob.glob(subdir + '\*.jpg'):
-#            print(filename)
-    valset_filenames = pd.read_csv("validationset_filenames.csv", encoding="ISO-8859-1")
+def check_validationset_missing_images():
+    with open('validationset_filenames.csv', 'rb') as f:
+        reader = csv.reader(f)
+        valset_filenames = list(chain.from_iterable(list(reader)))
+    
+    filenames = []
+    subdirs = [x[0] for x in os.walk("test_valset")]
+    for subdir in subdirs:
+        for filename in glob.glob(subdir + '\*.jpg'):
+            filenames.append(filename)
 
-check_images()
+    for i in valset_filenames:
+        count = 0
+        for j in filenames:
+            if i in j:
+                count = count + 1
+                break
+        if count == 0:
+            print(i)
+
+def check_trainset_missing_images():
+    with open('trainset_filenames.csv', 'rb') as f:
+        reader = csv.reader(f)
+        valset_filenames = list(chain.from_iterable(list(reader)))
+    
+    filenames = []
+    subdirs = [x[0] for x in os.walk("train_valset")]
+    for subdir in subdirs:
+        for filename in glob.glob(subdir + '\*.jpg'):
+            filenames.append(filename)
+
+    for i in valset_filenames:
+        count = 0
+        for j in filenames:
+            if i in j:
+                count = count + 1
+                break
+        if count == 0:
+            print(i)
+
+#check_validationset_missing_images()
+
+check_trainset_missing_images()
