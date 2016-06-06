8e33a9b84e493bcc3f038f4051870b38528991ef
DiedeKemper <diede.kemper.18@gmail.com>
2016-06-06 19:22:21 +0200

    Small changes. Logloss function does not need a scale parameter anymore.

--- Project2/Code/evaluation/tuneProbsMain.py
+++ Project2/Code/evaluation/tuneProbsMain.py
@@ -1,3 +1,12 @@
+'''
+Main script for tuning a submission file using a scale parameter. Saves the tuned submission file in a new csv file.
+Note: first find the optimal scale parameter.
+@author: Diede Kemper.
+'''
+
+
+
+
 from tune import tune_submissionfile
 import os
 
