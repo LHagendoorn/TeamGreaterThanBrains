7c4db334efd42db345ef9ef62079aecf03cf7bd4
Laurens Hagendoorn <Laurens.Hagendoorn@gmail.com>
2016-06-16 15:42:28 +0200

    more niceness (it's a real word!)

--- Project2/Code/Skin clusters/selectingSkinTestSet.py
+++ Project2/Code/Skin clusters/selectingSkinTestSet.py
@@ -3,6 +3,11 @@
 Created on Thu May 19 14:10:14 2016
 
 @author: Laurens
+
+This selects patches of colour within a certain range in the HSV colour space,
+these patches are correlated with the skin of the driver, and in theory they should
+give us three clusters, one for each hand(/arm) and one for the head. The position of the centre of these clusters
+is then taken as a feature for that image.
 """
 
 import csv
@@ -20,11 +25,12 @@ for f in files:
     km = KMeans(n_clusters = 3)
 
     im = cv2.imread(path + f)
-    
-    #im_ycrcb = cv2.cvtColor(im, cv2.COLOR_BGR2YCR_CB)
+    #Set upper and lower bounds for the skin colours:
     lower = np.array([0, 10, 15], dtype = "uint8")
     upper = np.array([50, 173, 240], dtype = "uint8")
+    #convert the image to HSV colour space
     converted = cv2.cvtColor(im, cv2.COLOR_BGR2HSV)
+    #apply the range, and perform a series of actions to smooth out the blobs
     skinMask = cv2.inRange(converted, lower, upper)
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (11, 11))
     skinMask = cv2.erode(skinMask, kernel, iterations = 1)
@@ -32,20 +38,16 @@ for f in files:
     skinMask = cv2.erode(skinMask, kernel, iterations = 1)
     skinMask = cv2.GaussianBlur(skinMask, (3, 3), 0)
     skinMask = np.array(skinMask)
+    #Prepare the skinMask for clustering
     w,h = skinMask.shape
     vals = np.reshape(skinMask,(w*h))
     x = range(w) * h
     y = np.repeat(range(h),w)
     skinCoords = np.vstack((vals,x,y)).T
+    #Fit 3 clusters to the skinMask, and store the cluster centres as features
     km.fit(skinCoords)
     clcenters = km.cluster_centers_
-    #print km.cluster_centers_
-    #print skinMask[skMask==1]
-    #cv2.imwrite(sys.argv[2], skinMask) # Second image
     
-    #skin = cv2.bitwise_and(im, im, mask = skinMask)
-    #cv2.imwrite('C:/Users/Laurens/Documents/Teamgreaterthanbrains/Project2/Code/skintest/MASK' + f, skinMask)         # Final image
-    #print np.reshape(np.concatenate(([f] *3,clcenters[:,1],clcenters[:,2])),(3,3)).T
     df = np.vstack((df,np.reshape(np.concatenate(([f] *3,clcenters[:,1],clcenters[:,2])),(3,3)).T))
 
 with open('skinTestFeatures.csv', 'wb') as csvfile:
